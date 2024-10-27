b60987389d333f37658d3e6b23fa8ecac398a88c
Vicky-codes17 <rameshvignesh0706@gmail.com>
2024-10-27 15:33:46 +0530

    DefaultValueOfPrimitiveDataTypes.java

---
+++ DefaultValueOfPrimitiveDataTypes/DefaultValueOfPrimitiveDataTypes.java
@@ -0,0 +1,20 @@
+public class DefaultValueOfPrimitiveDataTypes
+{
+    static byte a;
+    static short b;
+    static int i;
+    static char c;
+    static float d;
+    static double e;
+    static boolean bl;
+    public static void main(String[] args)
+    {
+        System.out.println("Default value of byte data type : "+a);
+        System.out.println("Default value of short data type : "+b);
+        System.out.println("Default value of int data type : "+i);
+        System.out.println("Default value of char data type : "+(int)c);
+        System.out.println("Default value of float data type : "+d);
+        System.out.println("Default value of double data type : "+e);
+        System.out.println("Default value of boolean data type : "+bl);
+    }
+}
